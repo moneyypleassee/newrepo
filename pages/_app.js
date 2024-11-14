6dbd757ead96c2232e2c3a14bb0d794468bdc3cd
Adam Rutkowski <adamrutkowski@Annies-MacBook-Air.local>
2024-11-14 15:32:56 -0800

    Fix: Added Clerk publishable key environment variable

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
-import { ClerkProvider } from '@clerk/clerk-react';
+import { ClerkProvider } from "@clerk/clerk-react";
+
+const clerkKey = process.env.NEXT_PUBLIC_CLERK_FRONTEND_API_KEY; // Adjust this variable name if necessary
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ClerkProvider frontendApi={process.env.NEXT_PUBLIC_CLERK_FRONTEND_API}>
+    <ClerkProvider publishableKey={clerkKey}>
       <Component {...pageProps} />
     </ClerkProvider>
   );
