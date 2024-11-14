7b3fd8f16e864042412114e2cd62516d6838e5cc
Adam Rutkowski <adamrutkowski@Annies-MacBook-Air.local>
2024-11-14 14:59:22 -0800

    Updated environment variable usage for Clerk

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,9 @@
-import '../styles/globals.css';
-import { ClerkProvider, RedirectToSignIn, SignedIn, SignedOut } from '@clerk/clerk-react';
+import { ClerkProvider } from '@clerk/clerk-react';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ClerkProvider frontendApi={process.env.NEXT_PUBLIC_CLERK_FRONTEND_API}>
-      <SignedIn>
-        <Component {...pageProps} />
-      </SignedIn>
-      <SignedOut>
-        <RedirectToSignIn />
-      </SignedOut>
+      <Component {...pageProps} />
     </ClerkProvider>
   );
 }
